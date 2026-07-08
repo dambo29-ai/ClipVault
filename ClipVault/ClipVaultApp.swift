63ec3403606a071c5f74d6431729cf5e3aaefbfd
Alejandro Mora <dambo29@mac.com>
2026-07-08 07:42:51 -0700

    Initial Commit

---
+++ ClipVault/ClipVaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipVaultApp.swift
+//  ClipVault
+//
+//  Created by Alejandro Mora on 7/8/26.
+//
+
+import SwiftUI
+
+@main
+struct ClipVaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
